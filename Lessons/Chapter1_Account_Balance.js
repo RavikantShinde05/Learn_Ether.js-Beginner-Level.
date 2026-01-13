14375ee5243117b90bbfa42b83b5a89794a01d5d
RevX05@ <117154752+RavikantShinde05@users.noreply.github.com>
2026-01-13 17:45:52 +0530

    Update Chapter1_Account_Balance.js

--- Lessons/Chapter1_Account_Balance.js
+++ Lessons/Chapter1_Account_Balance.js
@@ -1,13 +1,20 @@
 const { ethers } = require("ethers");
 
-const INFURA_ID = ''
-const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
+const INFURA_ID = ''// keep this in api key in .env file
 
-const address = '0x73BCEb1Cd57C711feaC4224D062b0F6ff338501e'
+// first get provider to read the data 
+const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`) 
+
+const address = '' // random address from etherscan.io
 
 const main = async () => {
-    const balance = await provider.getBalance(address)
-    console.log(`\nETH Balance of ${address} --> ${ethers.utils.formatEther(balance)} ETH\n`)
+    const balance = await provider.getBalance(address); // getbalance is a method
+    console.log("this is the balance in BigNumber", balance);
+
+    // to convert the balance into eth use this 
+    const balance_Eth = ethers.utils.formatEther(balance);
+    console.log("this balance is in the form of Eth", balance_Eth);
 }
 
 main()
+
